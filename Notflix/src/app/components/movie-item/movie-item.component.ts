86bdbd734b2b13f3e6ec0e7254cc4c14aea263d2
animan alloua yannhervee <yannhervee@gmail.com>
2025-01-06 01:44:20 -0500

    completed movie and movie list

---
+++ Notflix/src/app/components/movie-item/movie-item.component.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Movie } from '../../../services/interfaces/movie';
+
+@Component({
+  selector: 'app-movie-item',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './movie-item.component.html',
+  styleUrl: './movie-item.component.css'
+})
+export class MovieItemComponent {
+
+  @Input() movie!: Movie;
+
+  
+
+}
---
+++ Notflix/src/app/components/movie-list/movie-list.component.ts
@@ -0,0 +1,27 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { MovieItemComponent } from '../movie-item/movie-item.component';
+import { MovieService } from '../../../services/movie.service';
+import { Movie } from '../../../services/interfaces/movie';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-movie-list',
+  imports: [CommonModule, MovieItemComponent, FormsModule],
+  templateUrl: './movie-list.component.html',
+  styleUrl: './movie-list.component.css'
+})
+export class MovieListComponent implements OnInit{
+
+constructor(private movieService: MovieService){}
+movies: Movie[] =[]
+
+ngOnInit(): void {
+  this.movieService.getMovies().subscribe((response: Movie[]) => {
+    console.log("fetch result ", response)
+    this.movies = response; 
+  });
+
+
+}
+}
