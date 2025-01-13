043139d11c400309b896320ff5a3b6d75b0a3209
animan alloua yannhervee <yannhervee@gmail.com>
2025-01-12 23:57:54 -0500

    worked on select of movielist

---
+++ Notflix/src/app/components/movie/movie.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie',
+  imports: [],
+  templateUrl: './movie.component.html',
+  styleUrl: './movie.component.css'
+})
+export class MovieComponent {
+
+}
