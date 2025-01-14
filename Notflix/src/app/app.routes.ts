a6708e3359b62f3312f241f239df844f75b3c9da
animan alloua yannhervee <yannhervee@gmail.com>
2025-01-13 23:55:54 -0500

    added lazy loading

--- Notflix/src/app/app.routes.ts
+++ Notflix/src/app/app.routes.ts
@@ -8,11 +8,58 @@ import { MovieListComponent } from './components/movie-list/movie-list.component
 import { MovieComponent } from './components/movie/movie.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent }, // Default route (Home)
-    { path: 'login', component: LoginComponent },
-    { path: 'register', component: RegisterComponent },
-    { path: 'register-info', component: RegisterUsernameComponent },
-    { path: 'register-plan', component: RegisterPlanComponent },
-    { path: 'movie-list', component: MovieListComponent },
-    { path: 'movie-details', component: MovieComponent },
+    // { path: '', component: HomeComponent }, // Default route (Home)
+    // { path: 'login', component: LoginComponent },
+    // { path: 'register', component: RegisterComponent },
+    // { path: 'register-info', component: RegisterUsernameComponent },
+    // { path: 'register-plan', component: RegisterPlanComponent },
+    // { path: 'movie-list', component: MovieListComponent },
+    // { path: 'movie-details', component: MovieComponent },
+
+    { 
+        path: '', 
+        loadComponent: () => 
+          import('./components/home/home.component').then((m) => m.HomeComponent),
+      }, // Default route (Home)
+      { 
+        path: 'home', 
+        loadComponent: () => 
+          import('./components/home/home.component').then((m) => m.HomeComponent),
+      }, // Default route (Home)
+      { 
+        path: 'login', 
+        loadComponent: () => 
+          import('./components/login/login.component').then((m) => m.LoginComponent),
+      },
+      { 
+        path: 'register', 
+        loadComponent: () => 
+          import('./components/register/register.component').then((m) => m.RegisterComponent),
+      },
+      { 
+        path: 'register-info', 
+        loadComponent: () => 
+          import('./components/register-username/register-username.component').then(
+            (m) => m.RegisterUsernameComponent
+          ),
+      },
+      { 
+        path: 'register-plan', 
+        loadComponent: () => 
+          import('./components/register-plan/register-plan.component').then(
+            (m) => m.RegisterPlanComponent
+          ),
+      },
+      { 
+        path: 'movie-list', 
+        loadComponent: () => 
+          import('./components/movie-list/movie-list.component').then(
+            (m) => m.MovieListComponent
+          ),
+      },
+      { 
+        path: 'movie-details', 
+        loadComponent: () => 
+          import('./components/movie/movie.component').then((m) => m.MovieComponent),
+      },
 ];
